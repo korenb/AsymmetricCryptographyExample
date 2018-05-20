f8b829e3cc8e3db9ffedc10be96c3ee086ade18c
Sergey Korenyugin <korenb23@mail.ru>
2018-05-20 23:59:17 +0500

    Добавьте файлы проекта.

---
+++ Crypto/Human.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Crypto
+{
+    public class Human
+    {
+        public const int KEY_SIZE = 2048;
+        private RSAParameters secretKey;
+        public RSAParameters PublicKey { get; set; }
+
+        public Human()
+        {
+            using (var rsa = new RSACryptoServiceProvider(KEY_SIZE))
+            {
+                PublicKey = rsa.ExportParameters(false);
+                secretKey = rsa.ExportParameters(true);
+            }
+        }
+
+        public string Encrypt(string message, RSAParameters publicKey)
+        {
+            var dataBytes = Encoding.UTF8.GetBytes(message);
+
+            using (var rsa = new RSACryptoServiceProvider(KEY_SIZE))
+            {
+                rsa.ImportParameters(publicKey);
+                var encryptedData = rsa.Encrypt(dataBytes, true);
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
+
+        public string Decrypt(string data)
+        {
+            var dataBytes = Convert.FromBase64String(data);
+            using (var rsa = new RSACryptoServiceProvider(KEY_SIZE))
+            {
+                rsa.ImportParameters(secretKey);
+                var decryptedData = rsa.Decrypt(dataBytes, true);
+                return Encoding.UTF8.GetString(decryptedData);
+            }
+        }
+    }
+}
---
+++ Crypto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var roman = new Human();
+            var egor = new Human();
+
+            var message = "Ваше сообщение здесь";
+
+            Console.WriteLine("Есть сообщение: {0}", message);
+            var encrypted = roman.Encrypt(message, egor.PublicKey);
+            Console.WriteLine("\nРома шифрует сообщение с помощью публичного ключа Егора: ");
+            Console.WriteLine(Environment.NewLine + encrypted);
+            var decrypted = egor.Decrypt(encrypted);
+            Console.WriteLine("\nЕгор расшифровывает сообщение: {0}", decrypted);
+
+            Console.ReadKey();
+        }
+    }
+}
